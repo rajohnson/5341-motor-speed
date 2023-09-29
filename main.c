cbbd26fa224c64653a1efbe1f8e86721e535490e
Rob Johnson <johnson.rob.alan@gmail.com>
2023-09-28 20:20:11 -0700

    Calculate and display motor speed and torque

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 
 #include "project.h"
 
+#define R_SENSE (0.62)
+#define Ka      (0.3466)   // mv/min^-1
+#define Kt      (0.00331)  // mN*m/mA
+#define Vcc     (5.0)      // volts
+
 void lcd_init(void) {
     LCD_Char_1_Start();
 
@@ -56,33 +61,34 @@ int main(void) {
     Motor_emf_adc_Start();
     Rsense_adc_Start();
     lcd_init();
-    
+
     lcd_print_line_str(0, "Motor speed");
-    
+
+    motor_enable_Write(1);
+
     uint32_t loop_count = 0;
     while(1) {
         loop_count++;
 
-        if(loop_count == 1) {
-            motor_enable_Write(1);
-            lcd_print_line_str(0, "Motor speed: on");
-        }
-
-        if(loop_count == 500) {
-            // todo - read current sense resistor and display current
+        if(loop_count == 300) {
+            // read current sense resistor and display torque
             Rsense_adc_StartConvert();
             Rsense_adc_IsEndConversion(Rsense_adc_WAIT_FOR_RESULT);
             Rsense_adc_StopConvert();
             int16 Rsense_adc_count = Rsense_adc_GetResult16();
             int16 Rsense_mV = Rsense_adc_CountsTo_mVolts(Rsense_adc_count);
-            lcd_print_line_str_float(3, "Rsense mV ", Rsense_mV);
+            float current_mA = Rsense_mV / R_SENSE;
+            float torque_mNm = current_mA * Kt;
+            lcd_print_line_str_float(3, "Torque mNm:", torque_mNm);
         }
 
         if(loop_count == 2000) {
+            // read motor back emf and display speed
             int16 max_emf_reading = 0;
             motor_enable_Write(0);
             CyDelayUs(300);
 
+            // sample several times and save the max reading
             for(uint32_t i = 0; i < 700; ++i) {
                 Motor_emf_adc_StartConvert();
                 Motor_emf_adc_IsEndConversion(Motor_emf_adc_WAIT_FOR_RESULT);
@@ -94,11 +100,14 @@ int main(void) {
                 }
             }
 
-            float emf_V = Motor_emf_adc_CountsTo_Volts(max_emf_reading);
             motor_enable_Write(1);
-            lcd_print_line_str_float(1, "voltage:", emf_V);
-            lcd_print_line_str_float(2, "adc:", (float)max_emf_reading);
-            lcd_print_line_str(0, "Motor speed: on");
+
+            // convert back emf reading to speed
+            float emf_V = Vcc - Motor_emf_adc_CountsTo_Volts(max_emf_reading);
+            float emf_mV = emf_V * 1000;
+            float speed_rpm = emf_mV / Ka;
+            lcd_print_line_str_float(1, "back emf V:", emf_V);
+            lcd_print_line_str_float(2, "speed RPM:", speed_rpm);
             loop_count = 0;
         }
 
